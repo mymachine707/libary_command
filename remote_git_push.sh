95e500057a0cc944d11e679087b8ec1aa4bedc7e
mymachine707 <mymaxs441@gmail.com>
2022-12-19 10:17:01 +0500

    proto file added

---
+++ remote_git_push.sh
@@ -0,0 +1,27 @@
+cd getway_service/
+git add .
+git commit -m"Clean up news"
+git push
+cd ..
+
+cd category_service/
+git add .
+git commit -m"Clean up news"
+git push
+cd ..
+
+cd order_service/
+git add .
+git commit -m"Clean up news"
+git push
+cd ..
+
+cd authorization_service/
+git add .
+git commit -m"Clean up news"
+git push
+cd ..
+
+git add .
+git commit -m"Clean up news"
+git push
